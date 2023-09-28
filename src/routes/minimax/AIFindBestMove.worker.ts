73b36718cb2048442f412815139cfb26c4d37888
Daniel Reszka <dannn.r@gmail.com>
2023-09-28 20:37:04 +0200

    add time limit to AI

--- src/routes/minimax/AIFindBestMove.worker.ts
+++ src/routes/minimax/AIFindBestMove.worker.ts
@@ -14,8 +14,21 @@ onmessage = (event) => {
 function computeResponse(eventData: WorkerEventData) {
   const { aiDepth, moves, firstPlayer } = eventData
   const counter = writable(0)
-  const bestMove = AIFindBestMove(aiDepth, moves, firstPlayer, counter)
+
+  const startTime = Date.now()
+  const timeLimit = 7500 // Adjust time limit
+  let depth = 1
+  let bestMove
+
+  // This function will act as a break condition
+  const isTimeUp = () => Date.now() - startTime >= timeLimit
+
+  while (!isTimeUp()) {
+    const move = AIFindBestMove(depth, moves, firstPlayer, counter)
+    if (move) bestMove = move // Update bestMove if a move is found
+    depth++ // Increase depth for the next iteration
+  }
+
   const response = { data: bestMove, counter: get(counter) }
   return response
 }
-
